f75f4ae115d202697335f71064eecee9b75c6d2e
RandomlyKnighted <thughes3@jsu.edu>
2015-08-02 22:59:31 -0500

    Added humans.txt and started work on the models

---
+++ SideWaffleWebsite5/Models/Release.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SideWaffleWebsite5.Models
+{
+    public class Release
+    {
+        public string Name { get; set; }
+        public string TagName { get; set; }
+        public bool PreRelease { get; set; }
+    }
+}
